15e971253f01242a488d248735e345d8d921746f
jtos0003 <jtos0003@student.monash.edu>
2023-02-04 21:31:07 +1100

    feat: Added individual object images

--- src/scripts/load_object_mesh.py
+++ src/scripts/load_object_mesh.py
@@ -1,14 +1,10 @@
-import json
 import open3d as o3d
-import numpy as np
-import copy
 import os
 
-base_path = "/home/jason/object_grasp_dataset_paper/object_ds/"
+base_path = "~/~/Real-World-Object-and-Grasping-Dataset/object_ds/"
 objects = os.listdir(base_path)
 # sort in alphabetical order
 objects.sort()
 for obj in objects:
-    if "Resealable_Bags" in obj:
-        obj_mesh = o3d.io.read_triangle_mesh(base_path+obj)
-        o3d.visualization.draw_geometries([obj_mesh])
+    obj_mesh = o3d.io.read_triangle_mesh(base_path+obj)
+    o3d.visualization.draw_geometries([obj_mesh])
